16fae67af9bb66f36a54b60bc8df5633ecf8fff3
aieil <celph@protonmail.ch>
2018-04-02 11:33:42 -0400

    not outputting json

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python3 flow --model darkflow/cfg/yolov2.cfg --load darkflow/bin/yolov2.weights --gpu 1.0 --imgdir darkflow/img --json
+python3 flow --model darkflow/cfg/yolov2.cfg --load darkflow/bin/yolov2.weights --gpu 1.0 --imgdir darkflow/img
